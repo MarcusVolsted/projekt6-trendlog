dd849c9237ec60c2ac18cc08d8f84178d7029761
Mikkermand <Mikkermand@users.github.com>
2020-05-13 10:07:49 +0200

    History menu date picker add

--- js/calender.js
+++ js/calender.js
@@ -1,8 +1,17 @@
 $( function() {
-    $( "#datepicker" ).datepicker({
+    $( "#datepicker-history-menu1" ).datepicker({
       showOn: "button",
       buttonImage: "../img/calendar.png",
       buttonImageOnly: true,
       buttonText: "Select date"
     });
   } );
+
+  $( function() {
+      $( "#datepicker-history-menu2" ).datepicker({
+        showOn: "button",
+        buttonImage: "../img/calendar.png",
+        buttonImageOnly: true,
+        buttonText: "Select date"
+      });
+    } );
