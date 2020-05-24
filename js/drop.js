532a20df9a662a19b2b81ed7c9d7762acd07b6a1
Oliver-Zarp <60459243+Oliver-Zarp@users.noreply.github.com>
2020-05-24 13:54:31 +0200

    Add grup fix og web/mobil responsiv fix
    Add grup fix (via ændring af id'er), samt java rettelser
    
    web/mobil responsiv fix

--- js/drop.js
+++ js/drop.js
@@ -1,10 +1,11 @@
-   function addnewgroup() {
+// Add på alle fra web versionen  
+  
+  function addnewgroup() {
        var container = document.getElementById("group-add-select");
        var option = document.createElement("OPTION");
        option.innerHTML = document.getElementById("nyt-element-navn").value;
        option.setAttribute("selected","")
        container.options.add(option);
-       
    }
 
    function addnewgrouptwo() {
@@ -13,5 +14,72 @@
     option.innerHTML = document.getElementById("nyt-element-navn").value;
     option.setAttribute("selected","")
     container2.options.add(option);
-    
+}
+
+function addnewgroup1() {
+    var container = document.getElementById("group-add-select-out-1");
+    var option = document.createElement("OPTION");
+    option.innerHTML = document.getElementById("nyt-element-navn").value;
+    option.setAttribute("selected","")
+    container.options.add(option); 
+}
+
+function addnewgrouptwo1() {
+ var container2 = document.getElementById("group-add-select3");
+ var option = document.createElement("OPTION");
+ option.innerHTML = document.getElementById("nyt-element-navn").value;
+ option.setAttribute("selected","")
+ container2.options.add(option);
+}
+
+// Add på alle fra mobil versionen 
+
+function addnewgroupmobil() {
+    var container = document.getElementById("group-add-select");
+    var option = document.createElement("OPTION");
+    option.innerHTML = document.getElementById("nyt-element-navn-1").value;
+    option.setAttribute("selected","")
+    container.options.add(option);
+}
+
+function addnewgrouptwomobil() {
+ var container2 = document.getElementById("group-add-select-out");
+ var option = document.createElement("OPTION");
+ option.innerHTML = document.getElementById("nyt-element-navn-1").value;
+ option.setAttribute("selected","")
+ container2.options.add(option);
+}
+
+function addnewgroup1mobil() {
+ var container = document.getElementById("group-add-select-out-1");
+ var option = document.createElement("OPTION");
+ option.innerHTML = document.getElementById("nyt-element-navn-1").value;
+ option.setAttribute("selected","")
+ container.options.add(option); 
+}
+
+function addnewgrouptwo1mobil() {
+var container2 = document.getElementById("group-add-select3");
+var option = document.createElement("OPTION");
+option.innerHTML = document.getElementById("nyt-element-navn-1").value;
+option.setAttribute("selected","")
+container2.options.add(option);
+}
+
+// Add på alle fra new resources mobile/web 
+
+function addnewgroupresources() {
+    var container = document.getElementById("group-add-select-out-res");
+    var option = document.createElement("OPTION");
+    option.innerHTML = document.getElementById("nyt-element-navn-2").value;
+    option.setAttribute("selected","")
+    container.options.add(option);
+}
+
+function addnewgrouptworesources() {
+ var container2 = document.getElementById("group-add-select-res");
+ var option = document.createElement("OPTION");
+ option.innerHTML = document.getElementById("nyt-element-navn-2").value;
+ option.setAttribute("selected","")
+ container2.options.add(option);
 }
